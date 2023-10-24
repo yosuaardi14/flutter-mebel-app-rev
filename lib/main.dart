a24a44dad8f9f1caea7c1691b6c0d52293b7c542
yosuaardi14 <yosua.ardi14@gmail.com>
2023-10-24 21:04:04 +0700

    #2 -add initialroute check

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mebel_app_rev/app/data/services/auth_service.dart';
 import 'package:flutter_mebel_app_rev/app/data/services/local_storage_service.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:get/get.dart';
 import 'package:get_secure_storage/get_secure_storage.dart';
 import 'app/routes/app_pages.dart';
@@ -11,11 +13,21 @@ void main() async{
       options: DefaultFirebaseOptions.currentPlatform,
   );
   await GetSecureStorage.init(container: LocalStorageService.container);
+  var initialPage = await checkLogin();
   runApp(
     GetMaterialApp(
       title: "Mebel App",
-      initialRoute: AppPages.INITIAL,
+      initialRoute: initialPage,
       getPages: AppPages.routes,
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: const [FormBuilderLocalizations.delegate],
     ),
   );
+}
+
+Future<String> checkLogin() async {
+  AuthService authService = AuthService();
+  return await authService.loadLoginData()
+      ? Routes.LIST_PESANAN
+      : AppPages.INITIAL;
 }
